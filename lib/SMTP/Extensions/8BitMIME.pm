db7cee31920b95c5e054bbc0f000819ce0acbe16
Artur Kh <khrt@ya.ru>
2014-04-14 17:19:17 +0300

    NEW 8BitMIME extension

--- lib/SMTP/Extensions/8BitMIME.pm
+++ lib/SMTP/Extensions/8BitMIME.pm
@@ -3,4 +3,16 @@ package SMTP::Extensions::8BitMIME;
 use strict;
 use warnings;
 
+sub new {
+    my ($class, %args) = @_;
+    my $self = bless { %args }, $class;
+    $self;
+}
+
+sub ehlo { '8BITMIME' }
+
 1;
+
+__END__
+
+L<RFC6152|https://tools.ietf.org/html/rfc6152>
